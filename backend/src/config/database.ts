c79236412bffd8587771724253cf76a31728c11c
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-11-27 18:06:44 -0300

    refactor: complete backend configuration

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST || "postgres" || "localhost",
+  port: parseInt(process.env.PG_PORT || "5432" || "5433", 10),
+  database: process.env.PG_NAME || "tripdb",
+  user: process.env.PG_USER || "user",
+  password: process.env.PG_PASSWORD || "password",
 });
 
 export default pool;
